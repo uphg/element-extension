2278970893d120a1554f62c59c21fc176e87b476
uphg <1834805770@qq.com>
2022-08-22 14:26:54 +0800

    chore: 删除注释

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import path from 'path';
 import vue from '@vitejs/plugin-vue2'
 
-// const srcPath = `${path.resolve(__dirname, './src')}/`
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
