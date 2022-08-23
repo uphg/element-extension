6af2bcf289feaa31da24bb1d586ac751a64d348c
uphg <1834805770@qq.com>
2022-08-23 14:20:04 +0800

    test: 创建 table 测试文件

---
+++ test/table.spec.ts
@@ -0,0 +1,9 @@
+import { createLocalVue, mount } from "@vue/test-utils"
+import ElementPart from '../src/index'
+
+const localVue = createLocalVue()
+localVue.use(ElementPart)
+
+describe('table', () => {
+  
+})
