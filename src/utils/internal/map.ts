ae593dda12a0f54e74aaffc5035e23cabe2c4a7d
uphg <1834805770@qq.com>
2022-09-09 10:26:50 +0800

    refactor: 声明 map 函数类型

--- src/utils/internal/map.ts
+++ src/utils/internal/map.ts
@@ -1,12 +1,13 @@
-import isArray from "./isArray";
+import { ObjectLike } from "src/types/object-like";
+import isArray from "../isArray";
 
-function map(obj, callback) {
+export function map(obj: ObjectLike | any[], callback: (item: ObjectLike | any, index: number | string, obj: ObjectLike) => ObjectLike) {
   if(isArray(obj)) {
     const length = obj.length || 0
     const result = new Array(length)
     let index = -1
     while (++index < length) {
-      result[index] = callback(obj[index], index, obj)
+      result[index] = callback((obj as ObjectLike)[index], index, obj)
     }
     return result
   } else {
