81ba2837bfff5b019b3938de91cbecf7a1da500a
Lvheng <1834805770@qq.com>
2022-10-06 19:05:28 +0800

    refactor: 删除无用方法

--- src/utils/setAttrs.ts
+++
@@ -1,9 +0,0 @@
-import { each } from "./each"
-
-type Attrs = { [key: string]: string }
-
-function setAttrs(el: Element | HTMLElement, attrs: Attrs) {
-  each(attrs, (value, key) => el.setAttribute(key as string, value))
-}
-
-export default setAttrs
